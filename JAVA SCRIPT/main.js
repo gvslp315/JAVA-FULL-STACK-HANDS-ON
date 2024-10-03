16bdf191ec9622df894b685458203f4644823faa
Gubba <lakshmipathi.gubba@biworldwide.com>
2024-10-03 18:18:13 +0530

    second day java script commit

---
+++ JAVA SCRIPT/main.js
@@ -0,0 +1,6 @@
+// main.js
+
+import myFunction, { myVariable } from './mymodule.js';
+
+myFunction(); // This is the default function!
+console.log(myVariable); // 42
---
+++ JAVA SCRIPT/mymodule.js
@@ -0,0 +1,11 @@
+// myModule.js
+
+const myDefaultFunction = () => {
+    document.write("This is the default function!");
+  };
+  
+  const myVariable = 42;
+  
+  export default myDefaultFunction;
+  export { myVariable };
+  
